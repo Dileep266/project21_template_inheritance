2dba03786c45d8bab73e5a78ceb0fb6ffa3e9437
Dileep266 <somaladileep@gmail.com>
2023-12-05 15:26:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def parent(request):
+    return render(request,'parent.html')
+
+
+def child(request):
+    return render(request,'child.html')
+
+def child1(request):
+    return render(request,'child1.html')
+
+
+def parent1(request):
+    return render(request,'parent1.html')
+
+
+def child3(request):
+    return render(request,'child3.html')
